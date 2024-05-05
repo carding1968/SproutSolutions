b6b3c65e5368da5c1005f8313e321e7f6c987a97
christian calimutan <calimutan.christian196821@gmail.com>
2024-05-06 03:51:00 +0800

    fix startup issue

--- Sprout.Exam.DataAccess/obj/Release/netcoreapp3.1/Sprout.Exam.DataAccess.AssemblyInfo.cs
+++ Sprout.Exam.DataAccess/obj/Release/netcoreapp3.1/Sprout.Exam.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprout.Exam.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6163c1a93daa0631bc7648ec5d59581344cff910")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprout.Exam.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprout.Exam.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
